f865509c312f29704ef02d2dbaf19835f816b8e1
steve-dev-01 <stevemillerdev01@gmail.com>
2024-07-22 14:42:22 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
+  app.enableCors();
+
   app.setGlobalPrefix('api');
 
   await app.listen(3000);
